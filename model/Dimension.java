14effdd87313a7a52175556d2fa3fb35e569f89f
vince <vince@Spartan-1999>
2018-04-24 16:30:12 +0200

    Ma phase 2 est faite

---
+++ model/Dimension.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Dimension {
+	
+	private int width;
+	private int height;
+	
+	public Dimension(int width, int height) {
+		this.setWidth(width);
+		this.setHeight(height);
+	}
+	
+	public Dimension(Dimension dimension) {
+		this.setWidth(dimension.getWidth());
+		this.setHeight(dimension.getHeight());
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+}
