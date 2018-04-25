febb6b804edab9fa4985271573daf264b4696ee1
pcmga <pcmga@GAUTHIER-EXIA>
2018-04-25 09:45:38 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/controller/IOrderPerformer.java
@@ -0,0 +1,7 @@
+package jpu2016.dogfight.controller;
+
+public interface IOrderPerformer {
+
+	public void OrderPerform(UserOrder userOrder);
+	
+}
---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,19 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder implements IUserOrder{
+
+	private int player;
+	
+	public UserOrder(int player , Order order) {
+	
+	}
+	
+	public Order getOrder() {
+		return null;
+	}
+
+	public int getPlayer() {
+		return 1;
+	}
+	
+}
